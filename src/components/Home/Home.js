efd1d0ce6b25dc336d3ab4e6b9204ce1a49658c8
saurabh-gawade-29 <39211222+saurabh-gawade-29@users.noreply.github.com>
2022-03-05 13:12:41 +0530

    images add

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+  
+    };
+  }
+
+
   render() {
-    return (
-      <div>
-        <h1>this is home page</h1>
-      </div>
-    )
+    return (  
+      <h1>this is home page</h1>
+    );
   }
 }
 
-export default Home
+export default Home;
--- src/components/Sidenav/Sidenav.js
+++ src/components/Sidenav/Sidenav.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import { NavLink } from 'react-router-dom';
 import './Sidenav.css'
 
@@ -10,22 +9,22 @@ export class Sidenav extends Component {
       <>
         <ul className="nav flex-column nav-pills nav-fill">
           <li className="nav-item">
-            <NavLink to="/" className="nav-link px-0" aria-current="page" activeClassName="active">
+            <NavLink to="/" className="nav-link px-0" aria-current="page" activeclassname="active">
               Home
             </NavLink>
           </li>
           <li className="nav-item">
-            <NavLink to="/exchange" className="nav-link px-0" activeClassName="active">
+            <NavLink to="/exchange" className="nav-link px-0" activeclassname="active">
               Exchange
             </NavLink>
           </li>
           <li className="nav-item">
-            <NavLink to="/about" className="nav-link px-0" activeClassName="active">
+            <NavLink to="/about" className="nav-link px-0" activeclassname="active">
               About
             </NavLink>
           </li>
           <li className="nav-item">
-            <NavLink to="/contact" className="nav-link px-0" activeClassName="active">
+            <NavLink to="/contact" className="nav-link px-0" activeclassname="active">
               Contact
             </NavLink>
           </li>
