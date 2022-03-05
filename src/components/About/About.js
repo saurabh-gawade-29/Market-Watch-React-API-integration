4dbbe8e2264941466f9697ee1ba856d837c09b7f
saurabh-gawade-29 <39211222+saurabh-gawade-29@users.noreply.github.com>
2022-03-05 14:38:47 +0530

    About Dummy Ready

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,13 +1,40 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import "./About.css";
 
 class About extends Component {
   render() {
     return (
       <div>
-        <h1>this is about page</h1>
+        <div className="jumbotron jumbotron-fluid">
+          <div className="container">
+            <h1 className="display-4 about-head">MARKET WATCH</h1>
+            <p className="lead about-sub-head">
+              This is a modified jumbotron that occupies the entire horizontal
+              space of its parent.
+            </p>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat
+              omnis at beatae sequi distinctio amet minus iure architecto,
+              dolores suscipit ipsam similique qui dolore, facilis nulla. Error
+              tenetur temporibus nam? Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Sed tempore mollitia illo debitis! Dolorem quos
+              nulla, delectus cum possimus placeat et quaerat accusamus deleniti
+              doloribus illum quod error nam minus maxime. Facere cumque maiores
+              saepe. Vero assumenda unde tempora exercitationem labore animi at
+              natus excepturi! Impedit illo sunt atque eaque iusto illum error
+              dolore voluptatum facilis quidem esse saepe, perspiciatis
+              accusamus ex neque delectus dolorem exercitationem magnam.
+              Repellat incidunt consequatur sit exercitationem dicta, non
+              dolores rem consectetur asperiores, architecto quia deleniti
+              accusamus temporibus. Aut dolorem, perferendis porro libero optio
+              nesciunt illo quia facere animi veniam facilis sit rerum
+              architecto aspernatur!
+            </p>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default About
+export default About;
