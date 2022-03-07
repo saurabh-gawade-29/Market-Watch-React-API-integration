46fd84d4843a6190399e5224f878946aec47fbb5
saurabh-gawade-29 <39211222+saurabh-gawade-29@users.noreply.github.com>
2022-03-07 12:31:24 +0530

    Basic project done

--- src/components/Marketholiday/Marketholiday.js
+++ src/components/Marketholiday/Marketholiday.js
@@ -1,32 +1,25 @@
 import React, { Component } from "react";
+import moment from "moment";
 
 export class Marketholiday extends Component {
-    constructor(props) {
-      super(props)
-    
-      this.state = {
-         
-      }
-    }
-    
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
   render() {
     return (
       <div>
         <div className="card border-secondary mb-3">
           <div className="card-header">{this.props.date}</div>
           <div className="card-body text-secondary">
-            <h5 className="card-title">{this.props.name}</h5>
-            <p className="card-text">
-              {this.props.exchange}
-            </p>
+            <h5 className="card-title">Name: {this.props.name}</h5>
+            <p className="card-text">Exchange: {this.props.exchange}</p>
+            <p className="">Status: {this.props.status}</p>
+            <p className="">Open: {moment(this.props.open).format("h:mm a")}</p>
             <p className="">
-              {this.props.status}
-            </p>
-            <p className="">
-              {this.props.open}
-            </p>
-            <p className="">
-              {this.props.close}
+              Close: {moment(this.props.close).format("h:mm a")}
             </p>
           </div>
         </div>
