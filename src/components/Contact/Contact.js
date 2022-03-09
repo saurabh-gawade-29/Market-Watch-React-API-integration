2217b3bd97ae180ebb7d2d5c57b83e30f57b8b88
saurabh-gawade-29 <39211222+saurabh-gawade-29@users.noreply.github.com>
2022-03-09 12:24:41 +0530

    contact form state set

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -2,35 +2,69 @@ import React, { Component } from "react";
 import "./Contact.css";
 
 class Contact extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      fullName: "",
+      email: "",
+      msg: "",
+    };
+
+    this.contactSubmit = this.contactSubmit.bind(this);
+    this.contactOnChange = this.contactOnChange.bind(this);
+  }
+
+  contactSubmit = (event) => {
+    event.preventDefault();
+    alert(
+      `Hello: ${this.state.fullName} your contact name is ${this.state.email} your msg is ${this.state.msg} `
+    );
+  };
+
+  contactOnChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
   render() {
     return (
       <div>
         <div className="container">
           <div className="row">
             <div className="col-6">
-            <div className="mb-3">
-              <h1 className="contact-head p-1">Contact Us</h1>
-            </div>
+              <div className="mb-3">
+                <h1 className="contact-head p-1">Contact Us</h1>
+              </div>
               <div className="card px-4">
                 <div className="card-body">
-                  <form>
+                  <form onSubmit={this.contactSubmit}>
                     <div className="form-group my-2">
                       <label htmlFor="exampleInputEmail1">Full Name</label>
                       <input
+                        name="fullName"
                         type="text"
                         className="form-control"
-                        id="exampleInputEmail1"
                         placeholder="Enter your full name"
+                        value={this.state.fullName}
+                        onChange={(event) => {
+                          this.contactOnChange(event);
+                        }}
                       />
                     </div>
                     <div className="form-group my-2">
                       <label htmlFor="exampleInputEmail1">Email Address</label>
                       <input
+                        name="email"
                         type="email"
                         className="form-control"
-                        id="exampleInputEmail1"
                         aria-describedby="emailHelp"
                         placeholder="Enter email"
+                        value={this.state.email}
+                        onChange={(event) => {
+                          this.contactOnChange(event);
+                        }}
                       />
                       <small id="emailHelp" className="form-text text-muted">
                         We'll never share your email with anyone else.
@@ -41,9 +75,13 @@ class Contact extends Component {
                         Send Your Message
                       </label>
                       <textarea
+                        name="msg"
                         className="form-control"
-                        id="exampleFormControlTextarea1"
-                        rows="4"
+                        rows="5"
+                        value={this.state.msg}
+                        onChange={(event) => {
+                          this.contactOnChange(event);
+                        }}
                       ></textarea>
                     </div>
                     <button type="submit" className="btn btn-primary my-3">
